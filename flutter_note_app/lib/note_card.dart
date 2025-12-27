37c3ce62f173b2039989ddbcd09c1ae43e181dbb
Atu <arthur.lenoble4@etu.univ-lorraine.fr>
2025-12-27 13:43:20 +0100

    Ajout d'une barre de boutons pour sauvegarder et quitter la note

--- flutter_note_app/lib/note_card.dart
+++ flutter_note_app/lib/note_card.dart
@@ -11,34 +11,71 @@ class NoteCard extends StatelessWidget {
   Widget build(BuildContext context){
     var appState = context.watch<MyAppState>();
 
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        ElevatedButton(
-          onPressed: () => {
-            appState.accessNote(-1)
-          },
-          child: Icon(Icons.close)
-          ),
-        Align(
-          alignment: Alignment.center,
-          child: RichText( // Titre de la note
-            text: TextSpan(
-              text: "Titre de la note $index",
-              style: TextStyle(fontWeight: FontWeight.bold, color: Color.fromARGB(255, 242, 242, 242), fontSize: 24),
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          ButtonsRow(),
+          Align(
+            alignment: Alignment.center,
+            child: RichText( // Titre de la note
+              text: TextSpan(
+                text: "Titre de la note $index",
+                style: TextStyle(fontWeight: FontWeight.bold, color: Color.fromARGB(255, 242, 242, 242), fontSize: 24),
+              ),
             ),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: RichText(
+              text: TextSpan(
+                text: "Contenu complet de la page",
+                style:  TextStyle(color: Color.fromARGB(255, 242, 242, 242)),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ButtonsRow extends StatelessWidget {
+  const ButtonsRow ({super.key});
+  
+  @override
+  Widget build(BuildContext context){
+    var appState = context.watch<MyAppState>();
+    final boxSize = 40.0;
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        SizedBox( // Back button
+          height: boxSize,
+          width: boxSize,
+          child: FloatingActionButton(
+            onPressed: () => {
+              appState.accessNote(-1)
+            },
+            tooltip: "Retour",
+            child: Icon(Icons.close)
           ),
         ),
-        Align(
-          alignment: Alignment.center,
-          child: RichText(
-            text: TextSpan(
-              text: "Contenu complet de la page",
-              style:  TextStyle(color: Color.fromARGB(255, 242, 242, 242)),
-            ),
+        SizedBox( // Validate/Save button
+          height: boxSize,
+          width: boxSize,
+          child: FloatingActionButton(
+            onPressed: () => {
+              print("Sauvegarde")
+            },
+            tooltip: "Sauvegarder",
+            child: Icon(Icons.check)
           ),
-        )
+        ),
+
       ],
     );
   }
